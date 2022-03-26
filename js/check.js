06047fb3df8331567f7bae230ab133cf7d4555f9
Shaked Yunger <shakedyunger@gmail.com>
2022-03-26 13:30:23 +0300

    organzied files to folders

---
+++ js/check.js
@@ -0,0 +1,85 @@
+function isWin(hits) {
+  if (hits === 24) {
+    let modal = document.createElement("div");
+    modal.id = "modal";
+    document.body.appendChild(modal);
+    modal.innerText = "shaked has won";
+    return true;
+  }
+}
+
+function checkIfSpot() {
+  id = this.id;
+  let element = document.getElementById(id);
+  if (element.className === "myGuessinShipSpot") {
+    hits++;
+    isWin(hits);
+    this.id = "hit";
+    console.log(true);
+    return true;
+  } else {
+    this.id = "notHit";
+    console.log(false);
+    return false;
+  }
+}
+
+function ifCellInhabitVer(len, shipId, clickedCell) {
+  let ship = document.querySelector(shipId);
+  if (
+    Number(shipSpot.slice(1, 3)) < 14 - len &&
+    ship !== null &&
+    clickedCell.className === ""
+  ) {
+    let letter = shipSpot[0];
+
+    for (let index = 1; index < len; index++) {
+      let num = Number(shipSpot.slice(1, 3)) + index;
+      let cellIndex = document.querySelectorAll("#" + letter + num);
+
+      console.log(cellIndex[0]);
+      if (cellIndex[0].className !== "") {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+function ifCellInhabitHor(len, shipId, clickedCell) {
+  let ship = document.querySelector(shipId);
+  if (
+    Number(shipSpot.slice(1, 3)) < 14 - len &&
+    ship !== null &&
+    clickedCell.className === ""
+  ) {
+    let num = Number(clickedCell.id.slice(1, 3));
+    let letterIndex = LETTERS.indexOf(clickedCell.id[0]);
+
+    for (let index = 1; index < len; index++) {
+      let letters = LETTERS.indexOf(letterIndex + index);
+      let cellIndex = document.querySelectorAll(
+        "#" + LETTERS[letterIndex] + num
+      );
+
+      // console.log(index);
+      if (cellIndex[0] !== undefined) {
+        // console.log(index);
+        if (cellIndex[0] === "") {
+          return false;
+        }
+      } else {
+        console.log(index);
+        return false;
+      }
+      // if (cellIndex[0] !== undefined) {
+      //   if (cellIndex[0].className !== "") {
+      //     return false;
+      //   } else {
+      //     return true;
+      //   }
+      // }
+    }
+    return true;
+  }
+}
---
+++ js/play.js
@@ -0,0 +1,131 @@
+"use strict";
+
+let id = null;
+let hits = 0;
+let ShipLen = 0;
+
+const battleships = null;
+
+function setClickToships() {
+  let shipsNodeList = document.querySelectorAll(".ship");
+  shipsNodeList.forEach((element) =>
+    element.addEventListener("click", returnLen)
+  );
+}
+
+function returnLen() {
+  if (this.id === "twoLenShip") {
+    console.log(1);
+    ShipLen = 2;
+    return 2;
+  } else if (this.id === "threeLenShip") {
+    ShipLen = 3;
+    return 3;
+  } else if (this.id === "fourLenShip") {
+    ShipLen = 4;
+    return 4;
+  } else if (this.id === "fiveLenShip") {
+    ShipLen = 5;
+    return 5;
+  }
+}
+
+function setAttributesToOppoppnentsCell(elMyGuessingBoard) {
+  for (var i = 0, row; (row = elMyGuessingBoard.rows[i]); i++) {
+    for (var j = 0, col; (col = row.cells[j]); j++) {
+      if (LETTERS[i] === null || NUMBERS[j] === null) {
+        col.id = "null";
+      } else {
+        col.id = LETTERS[j] + NUMBERS[i];
+        col.addEventListener("click", checkIfSpot);
+      }
+    }
+  }
+}
+
+function setAttributesToMyCell(elMyBoard) {
+  for (var i = 0, row; (row = elMyBoard.rows[i]); i++) {
+    for (var j = 0, col; (col = row.cells[j]); j++) {
+      if (LETTERS[i] === null || NUMBERS[j] === null) {
+        col.id = "null";
+      } else {
+        col.id = LETTERS[j] + NUMBERS[i];
+        col.addEventListener("click", returnCellId);
+      }
+    }
+  }
+}
+
+function returnCellId() {
+  if (ShipLen === 2) {
+    placeShipsOnBoard(this, 2, "#twoLenShip", "rgb(187, 6, 6)");
+  } else if (ShipLen === 3) {
+    placeShipsOnBoard(this, 3, "#threeLenShip", "rgb(121, 39, 214)");
+  } else if (ShipLen === 4) {
+    placeShipsOnBoard(this, 4, "#fourLenShip", "rgb(52, 192, 39)");
+  } else if (ShipLen === 5) {
+    placeShipsOnBoard(this, 5, "#fiveLenShip", "rgb(154, 184, 23)");
+  }
+  createReadyButton();
+}
+
+function placeShipsOnBoard(clickedCell, len, shipId, color) {
+  shipSpot = clickedCell.id;
+  if (
+    direction === "vertical" &&
+    ifCellInhabitVer(len, shipId, clickedCell) === true
+  ) {
+    placeVerticaly(clickedCell, len, color, shipId);
+  } else if (
+    direction === "horizontal" &&
+    ifCellInhabitHor(len, shipId, clickedCell) === true
+  ) {
+    placeHorizontaly(clickedCell, len, color, shipId);
+  }
+}
+
+function placeVerticaly(clickedCell, len, color, shipId) {
+  shipsOnBoard++;
+  document.querySelectorAll(shipId)[0].remove();
+
+  for (let index = 1; index < len; index++) {
+    let numberIndex = Number(shipSpot.slice(1, 3)) + index;
+    let lettersIndex = shipSpot[0];
+    let cellIndex = document.querySelectorAll("#" + lettersIndex + numberIndex);
+
+    cellIndex[0].className = "myGuessinShipSpot";
+    cellIndex[1].className = "myShipSpot";
+    cellIndex[1].style.backgroundColor = color;
+  }
+
+  document.querySelectorAll("#" + clickedCell.id)[0].className =
+    "myGuessinShipSpot";
+  clickedCell.style.backgroundColor = color;
+  clickedCell.className = "myShipSpot";
+}
+
+function placeHorizontaly(clickedCell, len, color, shipId) {
+  console.log(123);
+  shipsOnBoard++;
+  document.querySelectorAll(shipId)[0].remove();
+  let indexOfLetter = LETTERS.indexOf(clickedCell.id[0]);
+
+  for (let index = 1; index < len; index++) {
+    let numberIndex = Number(shipSpot.slice(1, 3));
+    let lettersIndex = LETTERS[indexOfLetter + index];
+    let cellIndex = document.querySelectorAll("#" + lettersIndex + numberIndex);
+
+    cellIndex[0].className = "myGuessinShipSpot";
+    cellIndex[1].className = "myShipSpot";
+    cellIndex[1].style.backgroundColor = color;
+  }
+  document.querySelectorAll("#" + clickedCell.id)[0].className =
+    "myGuessinShipSpot";
+  clickedCell.style.backgroundColor = color;
+  clickedCell.className = "myShipSpot";
+}
+
+function callSetAtt() {
+  let elMyBoard = document.getElementById("myBoard");
+  setAttributesToMyCell(elMyBoard);
+}
